eea9e4e7136d709fd25e2bd4d6e6083e9dc381d1
Georgi Ivanov <gogbg@outlook.com>
2024-06-17 14:53:09 +0300

    wip

--- swagger-ui-dist/swagger-initializer.js
+++ swagger-ui-dist/swagger-initializer.js
@@ -2,7 +2,7 @@ window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
-  window.ui = SwaggerUI({
+  window.ui = SwaggerUIBundle({
     url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
